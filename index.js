2db501c9d45bdf1150f5e06eabde9fa1bca97aa4
İlyas Özkurt <ilyasozkurt@6harf.com>
2024-07-20 12:47:03 +0300

    Script optimized

--- index.js
+++ index.js
@@ -7,29 +7,26 @@ const THUMBNAILS_PATH = ROOT_PATH + "thumbnails/";
 const THUMBNAIL_SIZES = [100, 200, 300, 400, 500, 600, 700, 800, 900];
 
 exports.handler = async function (event, context) {
-
-    //Check if it's sns event
-    if (event.Records[0].EventSource === "aws:sns") {
-        event = JSON.parse(event.Records[0].Sns.Message);
-    }
-
-    console.log('Received S3 event:', JSON.stringify(event, null, 2));
-
-    const bucket = event.Records[0].s3.bucket.name;
-    const key = event.Records[0].s3.object.key;
-    const imageFileName = key.split('/').pop();
-
-    console.log(`Bucket: ${bucket}`, `Key: ${key}`);
-
     try {
+        // Check if it's SNS event and parse if necessary
+        if (event.Records[0].EventSource === "aws:sns") {
+            event = JSON.parse(event.Records[0].Sns.Message);
+        }
 
-        // get image from s3
-        const data = await s3.getObject({Bucket: bucket, Key: key}).promise();
-        let image = data.Body;
+        console.log('Received S3 event:', JSON.stringify(event, null, 2));
 
-        // resize image
-        for (const size of THUMBNAIL_SIZES) {
+        const bucket = event.Records[0].s3.bucket.name;
+        const key = event.Records[0].s3.object.key;
+        const imageFileName = key.split('/').pop();
 
+        console.log(`Bucket: ${bucket}`, `Key: ${key}`);
+
+        // Get the image from S3
+        const data = await s3.getObject({ Bucket: bucket, Key: key }).promise();
+        const image = data.Body;
+
+        // Resize and upload thumbnails concurrently
+        const thumbnailPromises = THUMBNAIL_SIZES.map(async (size) => {
             const resizedImage = await sharp(image)
                 .resize(size)
                 .withMetadata()
@@ -37,23 +34,28 @@ exports.handler = async function (event, context) {
 
             await s3.putObject({
                 Bucket: bucket,
-                Key: THUMBNAILS_PATH + size + '/' + imageFileName,
+                Key: `${THUMBNAILS_PATH}${size}/${imageFileName}`,
                 Body: resizedImage,
-                ContentType: "image"
+                ContentType: "image/jpeg" // Specify the correct MIME type
             }).promise();
 
             console.log(`Successfully resized image to ${size}x${size}`);
+        });
 
-        }
+        await Promise.all(thumbnailPromises);
 
-        console.log('Successfully resized image');
+        console.log('All thumbnails generated and uploaded successfully');
 
-        context.succeed(`Successfully resized image: ${key}`);
-
+        return {
+            statusCode: 200,
+            body: JSON.stringify('Image resized successfully'),
+        };
     } catch (err) {
+        console.error('Error resizing image:', err);
 
-        context.fail(`Error resizing image: ${err}`);
-
+        return {
+            statusCode: 500,
+            body: JSON.stringify('Error resizing image'),
+        };
     }
-
 };
