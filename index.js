5c31ea02e7a9215910ba5feb8424c341257f0c2c
İlyas Özkurt <ilyasozkurt@6harf.com>
2024-07-20 13:11:26 +0300

    Shitty over engineering closed.

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ exports.handler = async function (event, context) {
         return {
             statusCode: 200,
             body: JSON.stringify('Image resized successfully'),
+            imageFileName: imageFileName
         };
     } catch (err) {
         console.error('Error resizing image:', err);
